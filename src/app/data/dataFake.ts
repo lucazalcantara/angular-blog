d482faa61eb48ff911238d25a59b9e7f0904547d
lucazalcantara <lucasprestesmatos@hotmail.com>
2023-05-31 14:37:53 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "aaa",
+    "title": "NOVO HOMEM DE FERRO",
+    "description": "Marvel Studios anuncia novo filme do Homem de Ferro",
+    "photoCover": "https://www.imagensempng.com.br/wp-content/uploads/2021/08/07.png"
+  },
+  {
+    "id": "2",
+    "photo": "aaa",
+    "title": "Novas produções da fase 5 da Marvel anunciadas nesta manhã!",
+    "description": "ola ola ola zé",
+    "photoCover": "https://as2.ftcdn.net/v2/jpg/05/47/00/91/1000_F_547009195_LyF0xUG9AcHtQ3QrvLs9w7CgyRvVsV5x.jpg"
+  },
+]
